f0ed469e4d1e4762bfb5a44cd4a83855511aff2e
Valery Medvedev <walery.medvedev@gmail.com>
2025-04-27 17:58:12 +0300

    Реализована форма обратной связи

--- catalog/views.py
+++ catalog/views.py
@@ -5,4 +5,15 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        # Получаем данные из формы
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        print(f'Получено сообщение от {name}: телефон: {phone} и сообщение: {message}')
+
+        return render(request, 'contacts.html', {'name': name, 'phone': phone, 'message': message})
+
+    # Если метод запроса GET, то просто выводим страницу контактов
     return render(request, 'contacts.html')
