9cfdb8734b54c9772ab59e0f8318933c9f6929ce
Medvedevy <walery.medvedev@gmail.com>
2025-05-07 15:26:56 +0300

    коррекция

--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 
+from .models import Product
+
+
 def home(request):
-    return render(request, 'home.html')
+    # Выбираем последние 5 продуктов, сортируя по дате создания в обратном порядке
+    latest_products = Product.objects.order_by("-created_at")[:5]
+
+    # выволим их в консоль
+    print("Последние 5 добавленных продуктов")
+    for product in latest_products:
+        print(f"- {product.name} (Цена: {product.price})")
+
+    return render(request, "home.html")
 
 
 def contacts(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         # Получаем данные из формы
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
 
-        print(f'Получено сообщение от {name}: телефон: {phone} и сообщение: {message}')
+        print(f"Получено сообщение от {name}: телефон: {phone} и сообщение: {message}")
 
-        return render(request, 'contacts.html', {'name': name, 'phone': phone, 'message': message})
+        return render(request, "contacts.html", {"name": name, "phone": phone, "message": message})
 
     # Если метод запроса GET, то просто выводим страницу контактов
-    return render(request, 'contacts.html')
+    return render(request, "contacts.html")
