22910279b3a6ffc6bd5173977c8c580367ee588a
Medvedevy <walery.medvedev@gmail.com>
2025-05-07 15:26:13 +0300

    добавлена класс ContactInfo для добавления контактной информации

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,24 +1,30 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Category, Product
+from .models import Category, ContactInfo, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ('name',)
+    list_display = ("id", "name")
+    search_fields = ("name",)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category', 'image_display')
-    list_filter = ('category',)
-    search_fields = ('name', 'description')
-    readonly_fields = ('created_at', 'updated_at')
+    list_display = ("id", "name", "price", "category", "image_display")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
+    readonly_fields = ("created_at", "updated_at")
 
     def image_display(self, obj):
         if obj.image:
             return mark_safe(f'<img src="{obj.image.url}" width="50" height="50" />')
         return "-"
-    image_display.short_description = 'Изображение'
+
+    image_display.short_description = "Изображение"
+
+
+@admin.register(ContactInfo)
+class ContactInfoAdmin(admin.ModelAdmin):
+    list_display = ("country", "inn", "address")
