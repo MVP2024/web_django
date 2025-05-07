e76c434b53a919bffcbe08d8e3100e72f51d4cc1
Medvedevy <walery.medvedev@gmail.com>
2025-05-07 12:29:21 +0300

    замена created_at = models.DateTimeField(default=datetime.now, verbose_name='Дата создания') на created_at = models.DateTimeField(default=timezone.now, verbose_name='Дата создания') по рекомендуемой практике

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование', unique=True)
@@ -20,7 +20,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product/%Y/%m, %d', verbose_name='Изображение', null=True, blank=True)
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     price = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Цена за покупку', null=True, blank=True)
-    created_at = models.DateTimeField(default=datetime.now, verbose_name='Дата создания')
+    created_at = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
 
     def __str__(self):
@@ -30,4 +30,3 @@ class Product(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
         ordering = ['name']
-
