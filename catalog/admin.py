b7c8e2203bbedd05540e672c612caa62902f3ab8
Medvedevy <walery.medvedev@gmail.com>
2025-05-07 13:14:45 +0300

    Добавлена функция для отображения картинок продуктов в админке

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Category, Product
 
 
@@ -10,7 +12,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
+    list_display = ('id', 'name', 'price', 'category', 'image_display')
     list_filter = ('category',)
     search_fields = ('name', 'description')
     readonly_fields = ('created_at', 'updated_at')
+
+    def image_display(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="50" height="50" />')
+        return "-"
+    image_display.short_description = 'Изображение'
