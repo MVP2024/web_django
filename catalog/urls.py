8e4533478d89b2619c86f34b0d0a9a4d50a46176
Medvedevy <walery.medvedev@gmail.com>
2025-05-11 18:26:53 +0300

    СОздана страница, пути ,чтобы при выбора посмотрет товар в категории ,выводился весь товар

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home, product_detail
+from catalog.views import contacts, home, product_detail, category_list, product_list_by_category
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", home, name="home"),
     path("contacts/", contacts, name="contacts"),
-path("product/<int:pk>/", product_detail, name="product_detail"),
+    path("product/<int:pk>/", product_detail, name="product_detail"),
+    path("categories/", category_list, name="category_list"),
+path("category/<int:pk>/products/", product_list_by_category, name="product_list_by_category"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Product
+from .models import Product, ContactInfo, Category
 
 
 def home(request):
@@ -12,10 +12,14 @@ def home(request):
     for product in latest_products:
         print(f"- {product.name} (Цена: {product.price})")
 
-    return render(request, "home.html")
+        # Передаем список продуктов в контекст шаблона
+        return render(request, "home.html", {"latest_products": latest_products})
 
 
 def contacts(request):
+    # Получаем контактную информацию из базы данных
+    contact_info = ContactInfo.objects.first()
+
     if request.method == "POST":
         # Получаем данные из формы
         name = request.POST.get("name")
@@ -24,7 +28,38 @@ def contacts(request):
 
         print(f"Получено сообщение от {name}: телефон: {phone} и сообщение: {message}")
 
-        return render(request, "contacts.html", {"name": name, "phone": phone, "message": message})
+        # Перел/даём контактную информацию и данные в контекст
+        return render(request, "contacts.html",
+                      {"contact_info": contact_info, "name": name, "phone": phone, "message": message})
 
     # Если метод запроса GET, то просто выводим страницу контактов
-    return render(request, "contacts.html")
+    return render(request, "contacts.html", {"contact_info": contact_info})
+
+
+def product_detail(request, pk):
+    # Получаем продукт по pk или возвращаем ошибку 404
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+    "product": product
+    }
+    return render(request, "product_detail.html", context)
+
+
+def category_list(request):
+    categories = Category.objects.all() # Получаем все категории
+    context = {
+        "categories": categories # Передаем их в контекст
+    }
+    return render(request, "category_list.html", context)
+
+
+def product_list_by_category(request, pk):
+    # Получаем категорию по pk или возвращаем ошибку 404
+    category = get_object_or_404(Category, pk=pk)
+    # Получаем все продукты, связанные с этой категорией
+    products = Product.objects.filter(category=category)
+    context = {
+        "category": category,
+        "products": products
+    }
+    return render(request, "product_list_by_category.html", context)
