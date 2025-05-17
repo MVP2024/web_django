bd0862c58d4f438229299f10b1cdbbbb4389c1df
Medvedevy <walery.medvedev@gmail.com>
2025-05-17 16:47:36 +0300

    Создано новое приложение blog

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL") # Добавляем slug для SEO
+    content = models.TextField(verbose_name="Содержимое")
+    preview = models.ImageField(upload_to="blog_previews/%Y/%m/%d/", verbose_name="Превью", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    is_published = models.BooleanField(default=False, verbose_name="Опубликовано")
+    views_count = models.IntegerField(default=0, verbose_name="Количество просмотров")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'slug': self.slug})
+
+    class Meta:
+        verbose_name = "Запись блога"
+        verbose_name_plural = "Записи блога"
+        ordering = ["-created_at"]
---
+++ blog/views.py
@@ -0,0 +1,69 @@
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.shortcuts import get_object_or_404
+from django.utils.text import slugify
+
+from .models import Post
+from .forms import PostForm
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.filter(is_published=True) # Показываем только опубликованные посты
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+    slug_field = 'slug' # Указываем, что используем slug для поиска объекта
+
+    def get_object(self, queryset=None):
+        # Получаем объект и увеличиваем счетчик просмотров
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog:list') # Перенаправление после успешного создания
+
+    def form_valid(self, form):
+        # Автоматически генерируем slug, если он не был введен
+        if not form.cleaned_data.get('slug'):
+            form.instance.slug = slugify(form.cleaned_data['title'])
+        return super().form_valid(form)
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    slug_field = 'slug' # Указываем, что используем slug для поиска объекта
+
+    def get_success_url(self):
+        # Перенаправляем на страницу деталей поста после обновления
+        return reverse_lazy('blog:detail', kwargs={'slug': self.object.slug})
+
+    def form_valid(self, form):
+        # Автоматически генерируем slug, если он не был введен или изменился заголовок
+        if not form.cleaned_data.get('slug') or form.cleaned_data.get('slug') != slugify(form.cleaned_data['title']):
+             form.instance.slug = slugify(form.cleaned_data['title'])
+        return super().form_valid(form)
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:list') # Перенаправление после успешного удаления
+    context_object_name = 'post'
+    slug_field = 'slug' # Указываем, что используем slug для поиска объекта
