57732779a851da32194f3f77abb602e1acb1646d
Medvedevy <walery.medvedev@gmail.com>
2025-05-30 16:08:19 +0300

    Добавлены константа `FORBIDDEN_WORDS` и методы `clean_name`, `clean_description` для проверки запрещенных слов. Реализован метод `clean_price` для валидации отрицательной цены.

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,7 @@
 # File: D:/Project/web_django-develop/catalog/forms.py
 
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Product
 
@@ -9,7 +10,21 @@ class ProductForm(forms.ModelForm):
     """
     Форма для создания и редактирования продуктов.
     Использует модель Product и настраивает виджеты для полей формы.
+    Добавлена валидация для полей 'name' и 'description' на наличие запрещенных слов.
     """
+
+    FORBIDDEN_WORDS = [
+        "казино",
+        "криптовалюта",
+        "крипта",
+        "биржа",
+        "дешево",
+        "дёшево",
+        "бесплатно",
+        "обман",
+        "полиция",
+        "радар",
+    ]
 
     class Meta:
         model = Product
@@ -21,6 +36,36 @@ class ProductForm(forms.ModelForm):
             "category": forms.Select(attrs={"class": "form-select"}),  # Используем form-select для select
             "price": forms.NumberInput(attrs={"class": "form-control"}),
         }
+
+    def clean_name(self):
+        """
+        Валидация поля 'name' на наличие запрещённых слов
+        """
+        name = self.cleaned_data["name"]
+        for word in self.FORBIDDEN_WORDS:
+            if word in name.lower():
+                raise ValidationError(f"Слово '{word}' запрещено в описании продукта!")
+        return name
+
+    def clean_description(self):
+        """
+        Валидация поля 'description' на наличие запрещённых слов
+        """
+        description = self.cleaned_data["description"]
+        if description:
+            for word in self.FORBIDDEN_WORDS:
+                if word in description.lower():
+                    raise ValidationError(f"Слово '{word}' запрещено в описании продукта!")
+            return description
+
+    def clean_price(self):
+        """
+        Валидация поля 'price' на отсутствие отрицатеьных значений.
+        """
+        price = self.cleaned_data["price"]
+        if price is not None and price < 0:
+            raise ValidationError("Цена продукта не может быть отрицательной!")
+        return price
 
 
 class ContactForm(forms.Form):
